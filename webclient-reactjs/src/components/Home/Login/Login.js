5f5622fc5e3e43a812fe49f79bb00b9a3133ed4b
Ahkeem Lang <langahkeem@gmail.com>
2019-12-05 12:03:51 -0600

    developed necessary methods

--- webclient-reactjs/src/components/Home/Login/Login.js
+++ webclient-reactjs/src/components/Home/Login/Login.js
@@ -6,16 +6,28 @@ class Login extends Component {
 
   handleChange = e => {
     this.setState({
-      name: e.target.value
+      [e.target.name]: e.target.value
     })
   }
 
-  // handleSignIn = (e) => {
-  //   e.preventDefault();
-  //   let username = this.refs.username
-  //   let password = this.refs.password
-  //   this.props.onSignIn(username, password)
-  // }
+
+  handleLogin = (data) => {
+    this.setState({
+      user: data
+    })
+  }
+
+  handleSuccessfulAuth = (data) => {
+    this.props.handleLogin(data);
+    this.props.history.push("/auth/login");
+  }
+
+  handleSignIn = (e) => {
+    e.preventDefault();
+    let username = this.refs.username
+    let password = this.refs.password
+    this.props.onSignIn(username, password)
+  }
 
   handleSubmit = e => {
     const { email, password } = this.state;
@@ -23,13 +35,15 @@ class Login extends Component {
     axios.post(
       "http://localhost:9005/auth/login",
       {
-         
-          email: email,
-          password: password
+       
+        email: email,
+        password: password
+          
         
       },
       { withCredentials: true }
     ).then(res => {
+      console.log("yay!")
       if (res.data.logged_in) {
         this.props.handleSuccessfulAuth(res.data)
       }
@@ -45,7 +59,7 @@ class Login extends Component {
           <h1>MyPass</h1>
         <div className="header">Login</div>
           <div className="content">
-            <form className="form" onSubmit={this.handleSubmit}>
+            <form className="form" onSubmit={this.handleSuccessfulAuth}>
               <Row>
                 <Col xs={8} sm={8} md={12} lg={12}>
                   <div className="form-group">
@@ -73,7 +87,7 @@ class Login extends Component {
                   </div>
                 </Col>
               </Row>
-                <input type="submit" className="btn" value="Login" />
+                <input type="submit" className="btn" value="Login" onSubmit={this.handleSubmit} />
                 <button className="btn" type="button" onClick={ () => this.changeView("signUp")}>Create an Account</button>
             </form>
           </div>
